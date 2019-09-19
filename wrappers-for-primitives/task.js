0dda4fcdfb5523e6bd14d10e18127b20051a003f
Ivan <taran@nxt.ru>
2019-09-19 14:55:34 +0300

    Did homework

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,9 +12,45 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    if (parseFloat(percent) >= 0 || parseFloat(percent)) {
+        percent = parseFloat(percent) / 100;
+    } else {
+        return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    }
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    if (parseFloat(contribution) >= 0 || parseFloat(contribution)) {
+        contribution = parseFloat(contribution);
+    } else {
+        return `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
+    }
+
+    if (parseFloat(amount) >= 0 || parseFloat(amount)) {
+        amount = parseFloat(amount);
+    } else {
+        return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
+    }
+
+    if (!date) {
+        return `Параметр "Срок ипотеки" содержит неправильное значение ${date}`;
+    }
+
+    let numberMonths = Math.round((Date.parse(date) / (86400 * 30 * 1000)) - ((new Date().getTime()) / (86400 * 30 * 1000)));
+
+    if(numberMonths <= 0 ) {
+        return `Параметр "Дата" содержит неправильное значение ${date}`;
+    }
+
+    let totalAmount = amount - contribution;
+
+    if (totalAmount > 0) {
+        let sumMonth = sumCredit * ((percent * 1 / 12)+(percent * 1 / 12)/(((1+(percent * 1 / 12)) ** numberMonths) -1 ));
+        totalAmount = (sumMonth * numberMonths + contribution).toFixed(2);
+    } else {
+        totalAmount = 0;
+    }
+
+    console.log(totalAmount);
+    return totalAmount;
 }
 
 function sayHello() {
@@ -23,6 +61,14 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    let greeting;
+
+    if(typeof name === 'string' && name.length > 1 && name != 'null' && name != 'undefined'){
+        greeting = `Привет, мир! Меня зовут ${name}.`;
+    } else {
+        greeting = `Привет, мир! Меня зовут Аноним.`;
+    }
+
+    console.log(greeting);
+    return greeting;
 }
